c3a47ed3e0e8ad3280f8fe40031ecd3637d889d9
Matin <mnasirii829@gmail.com>
2023-05-30 15:50:28 +0330

    New ReadMD File

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Welcome to my Java
-## My `Skills`
-  test copy
+ Do you want to👨‍💻learn Java?
+-
+- - - 
+### So Let's Get Started!
+
+>To start with the options above, take a `fork` of my project and `clone` it on your computer.
+
+Start Learning With ME!
--- src/E_Instance/A38_StaticUsage.java
+++ src/E_Instance/A38_StaticUsage.java
@@ -3,10 +3,10 @@ package E_Instance;
 public class A38_StaticUsage {
     public void start(){
 
-     String name = A38_StaticUsage2 .name;
+     String name = A38_StaticUsage2 .MyName();
         System.out.println(name);
 
-    String MyName = A38_StaticUsage2 .MyName();
+    String MyName = A38_StaticUsage2 .name;
         System.out.println(MyName);
     }
 }
